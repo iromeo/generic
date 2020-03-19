f4b5b52059e27e822bc0cc202c9e64edaa30cfac
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-03-19 15:12:30 +0300

    feat: Customize tmpdir
    Now you could use __LSF_JOB_CUSTOM_TMPDIR__ env variable to override custom directory for job. It could be a workaround for
    https://github.com/ewels/MultiQC/issues/806 issue. E.g our LSF jobs based on docker containers ignores TMPDIR setting in docker environment variable files, at some moment it is being replaced with '/tmp'. Standard LSF property $__LSF_JOB_TMPDIR__ couldn't help because it is set by LSF and also refers to tmp folder in the other file system. So you could set __LSF_JOB_CUSTOM_TMPDIR__ in your docker env variables file + make job non-local

--- {{cookiecutter.profile_name}}/lsf-jobscript.sh
+++ {{cookiecutter.profile_name}}/lsf-jobscript.sh
@@ -3,9 +3,21 @@
 # invoke with --jobscript
 
 # Propagate TMPDIR for LSF nodes:
-if [ -n "$__LSF_JOB_TMPDIR__" ]; then
-  export TMPDIR=$__LSF_JOB_TMPDIR__
+
+if [ -z "$__LSF_JOB_CUSTOM_TMPDIR__" ]
+then
+  # $__LSF_JOB_CUSTOM_TMPDIR__ is empty or not set:
+  if [[ ! -z "$__LSF_JOB_TMPDIR__" ]]; then
+    echo 'Set TMPDIR from $__LSF_JOB_TMPDIR__'
+    export TMPDIR=$__LSF_JOB_TMPDIR__
+  fi
+else
+  # # $__LSF_JOB_CUSTOM_TMPDIR__ is set and NOT empty
+  export TMPDIR=$__LSF_JOB_CUSTOM_TMPDIR__
+  echo 'Set TMPDIR from $__LSF_JOB_CUSTOM_TMPDIR__'
 fi
+
+echo "Job uses tempdir: TMPDIR=$TMPDIR"
 
 # Snakemake job properties:
 
